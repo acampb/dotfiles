b1b14105e2a0cb2d19230341fd83be3fbcc2e015
acampb <andrewbcampbell@outlook.com>
2023-01-02 12:20:23 -0500

    try copying font

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+
 # install JetBrains Mono font on Linux
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
+cp ~/.local/share/fonts/fonts/ttf/JetBrainsMono-Regular.ttf ~/.local/share/fonts/
